4437bb3b454c031a70405e0a225ae1618d127b10
Guldana <g_rishadkyzy@kbtu.kz>
2025-10-03 21:20:48 +0500

    1 commit

---
+++ lab4/lab4H.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left, *right; 
+    Node(int data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    } 
+};
+
+class BST {
+public:
+    Node* root;
+    
+    BST() {
+        root = NULL;
+    }
+    
+    Node* insert(Node* node, int data) {
+        if(node == NULL) {
+            return new Node(data);
+        }
+        if(data < node->data) {
+            node->left = insert(node->left, data);
+        }
+        else if(data > node->data) {
+            node->right = insert(node->right, data);
+        }
+        return node;      
+    }
+
+void inOrder(Node *node) {
+        if (node == NULL)
+            return;
+        inOrder(node->left);
+        cout << node->data << " ";
+        inOrder(node->right);
+    }
+
+    void transform(Node* node, int& sum){
+        if(node == NULL) return ;
+        transform(node->right, sum);
+        sum += node->data ;
+        node->data = sum ;
+        transform(node->left, sum);
+    }
+    void reverseInOrder(Node *node) {
+    if (node == NULL) return;
+    reverseInOrder(node->right);
+    cout << node->data << " ";
+    reverseInOrder(node->left);
+}
+
+
+    
+};
+
+int main(){
+    int n ; 
+    cin >> n;
+    BST bst;
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        bst.root = bst.insert(bst.root, val);
+    }
+    int sum = 0 ;
+    bst.transform(bst.root, sum);
+    
+    bst.reverseInOrder(bst.root);
+
+}
