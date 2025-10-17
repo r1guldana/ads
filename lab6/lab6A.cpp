18980e7e623b75fe868a5c3b62050d67bd88e819
Guldana <g_rishadkyzy@kbtu.kz>
2025-10-17 18:17:35 +0500

    1 commit

--- lab6/lab6A.cpp
+++ lab6/lab6A.cpp
@@ -3,58 +3,99 @@
 #include <string>
 using namespace std;
 
-string a[1000] ;
+void merge(vector<char> &arr, int left, int mid, int right)
+{
+    int n1 = mid - left + 1; // left subarray size
+    int n2 = right - mid;    // right subarray
 
-void merge(char l, int m, char r) {
-    int ll = 0 ;
-    int rr = m ;;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1];
-    int R[n2];
-    for (int i = 0; i < n1; i++) {
-        L[i] = a[l + i];
+    vector<char> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        R[i] = arr[mid + 1 + i];
     }
 
-    for (int i = 0; i < n2; i++) {
-        R[i] = a[m + 1 + i];
-    }
+    int i = 0;
+    int j = 0;
+    int k = left;
 
-    int uk1 = 0;
-    int uk2 = 0;
-    int k = l;
-    while (uk1 < n1 && uk2 < n2) {
-        if (L[uk1] <= R[uk2]) {
-            a[k] = L[uk1];
-            uk1++;
-        } else {
-            a[k] = R[uk2];
-            uk2++;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
         }
         k++;
     }
 
-    while (uk1 < n1) {
-        a[k] = L[uk1];
-        uk1++; k++;
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
     }
-    while (uk2 < n2) {
-        a[k] = L[uk2];
-        uk2++; k++;
-    }
-
-}
-
-void mergesort(char l, char r) {
-    if (l < r ) {
-        int m = ( + ) / 2;
-        mergesort(l, m);
-        mergesort(m + 1, r);
-        merge(l, m, r);
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
- 
-int main(){
-    string n ;
-    cin >> n ;
+void mergeSort(vector<char> &arr, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+void PrintArr(vector<char> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i];
+    }
+}
+bool isVowel(char ch)
+{
+    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    string alphabet;
+    cin >> alphabet;
+    vector<char> vowel;
+    vector<char> consonants;
+    vector<char> alph;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (isVowel(alphabet[i]))
+        {
+            vowel.push_back(alphabet[i]);
+        }
+        else
+        {
+            consonants.push_back(alphabet[i]);
+        }
+    }
+    mergeSort(vowel, 0, vowel.size() - 1);
+    mergeSort(consonants, 0, consonants.size() - 1);
+    PrintArr(vowel);
+    PrintArr(consonants);
 }
